1633745569b5f3dffbd827cca14093ea89fd77b2
Gyenh <blazeog@hotmail.com>
2018-08-29 14:16:19 +0200

    adding user id in order tab

--- db/migrate/20180828124003_create_orders.rb
+++ db/migrate/20180828124003_create_orders.rb
@@ -1,9 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.string :name, null: false
-      t.text :address, null: false
-      t.string :email, null: false
+      t.references :user
       t.integer :status, null: false, default: 0
       t.references :cart, foreign_key: true
       t.decimal :amount, precision: 6, scale: 2
